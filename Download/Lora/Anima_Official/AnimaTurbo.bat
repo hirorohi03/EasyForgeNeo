8265d9059513ca4905a9fe25bda893ac110de65b
hirorohi <227736546+hirorohi03@users.noreply.github.com>
2026-05-27 08:20:25 +0900

    Update AnimaTurbo.bat

--- Download/Lora/Anima_Official/AnimaTurbo.bat
+++ Download/Lora/Anima_Official/AnimaTurbo.bat
@@ -5,7 +5,7 @@ set CIVITAI_MODEL_DOWNLOAD=%EASY_TOOLS%\Civitai\Civitai_ModelDownload.bat
 pushd %~dp0..\..\..\Model\Lora
 
 @REM https://civitai.red/models/2560840/
-call %CIVITAI_MODEL_DOWNLOAD% Anima_Official\ anima-turbo-lora-v0.1.safetensors 2560840 2877687
+call %CIVITAI_MODEL_DOWNLOAD% Anima_Official\ anima-turbo-lora-v0.2.safetensors 2560840 2979642
 if %ERRORLEVEL% neq 0 ( popd & exit /b 1 )
 
 popd rem %~dp0..\..\..\Model\Lora
