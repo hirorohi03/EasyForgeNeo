cd44a5e664e808bbdc1f257a142e3cd62d3aa06b
hirorohi <227736546+hirorohi03@users.noreply.github.com>
2026-06-08 10:12:36 +0900

    Update Anima_Custom Downloader

--- Download/DiffusionModels/Anima_Custom/botan_anima.bat
+++ Download/DiffusionModels/Anima_Custom/botan_anima.bat
@@ -5,7 +5,7 @@ set CIVITAI_MODEL_DOWNLOAD=%EASY_TOOLS%\Civitai\Civitai_ModelDownload.bat
 pushd %~dp0..\..\..\Model\DiffusionModels
 
 @REM https://civitai.red/models/2518069/
-call %CIVITAI_MODEL_DOWNLOAD% Anima_Custom\ botanAnima_base10V14.safetensors 2518069 2989952
+call %CIVITAI_MODEL_DOWNLOAD% Anima_Custom\ botanAnima_base10V18.safetensors 2518069 3008292
 if %ERRORLEVEL% neq 0 ( popd & exit /b 1 )
 
 popd rem %~dp0..\..\..\Model\DiffusionModels
