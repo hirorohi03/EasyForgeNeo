bee298402884f50b3d6b1122db80e1bac4f13b16
hirorohi <227736546+hirorohi03@users.noreply.github.com>
2026-07-21 10:21:39 +0900

    Update Anima_Custom Downloader

--- Download/DiffusionModels/Anima_Custom/AnimaIka.bat
+++ Download/DiffusionModels/Anima_Custom/AnimaIka.bat
@@ -5,7 +5,7 @@ set CIVITAI_MODEL_DOWNLOAD=%EASY_TOOLS%\Civitai\Civitai_ModelDownload.bat
 pushd %~dp0..\..\..\Model\DiffusionModels
 
 @REM https://civitai.red/models/2426265/
-call %CIVITAI_MODEL_DOWNLOAD% Anima_Custom\ animaika_v43.safetensors 2426265 3062148
+call %CIVITAI_MODEL_DOWNLOAD% Anima_Custom\ animaika_v44.safetensors 2426265 3115541
 if %ERRORLEVEL% neq 0 ( popd & exit /b 1 )
 
 popd rem %~dp0..\..\..\Model\DiffusionModels
--- Download/DiffusionModels/Anima_Custom/hassakuAnima.bat
+++ Download/DiffusionModels/Anima_Custom/hassakuAnima.bat
@@ -5,7 +5,7 @@ set CIVITAI_MODEL_DOWNLOAD=%EASY_TOOLS%\Civitai\Civitai_ModelDownload.bat
 pushd %~dp0..\..\..\Model\DiffusionModels
 
 @REM https://civitai.red/models/2641326/
-call %CIVITAI_MODEL_DOWNLOAD% Anima_Custom\ hassakuAnima_v1.safetensors 2641326 3016509
+call %CIVITAI_MODEL_DOWNLOAD% Anima_Custom\ hassakuAnima_v12.safetensors 2641326 3144783
 if %ERRORLEVEL% neq 0 ( popd & exit /b 1 )
 
 popd rem %~dp0..\..\..\Model\DiffusionModels
